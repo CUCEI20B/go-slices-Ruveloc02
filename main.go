fd8e3422f641bf69c127c37006c57004a5f8d15b
Carlos Ruiz Velasco Lopez <carlos.ruizvelasco2917@alumnos.udg.mx>
2020-10-12 20:21:10 -0500

    Primera prueba

--- main.go
+++ main.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var n int
+	var suma float64
+	fmt.Scan(&n)
+	numeros := make([]float64, n)
+	for f := 0; f < len(numeros); f++ {
+		fmt.Scan(&numeros[f])
+		suma = suma + numeros[f]
+	}
+	fmt.Println(suma)
 }
